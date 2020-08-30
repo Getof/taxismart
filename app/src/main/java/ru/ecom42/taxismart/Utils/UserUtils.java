c072bfae2dcc3ec967aa5cbaa64417c25ee970e4
Getof <bohonovdenis1980@gmail.com>
2020-08-30 09:22:00 +0700

    up

---
+++ app/src/main/java/ru/ecom42/taxismart/Utils/UserUtils.java
@@ -0,0 +1,56 @@
+package ru.ecom42.taxismart.Utils;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import ru.ecom42.taxismart.Common;
+import ru.ecom42.taxismart.Model.TokenModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Map;
+
+public class UserUtils {
+    public static void updateUser(View view, Map<String, Object> updateData) {
+        FirebaseDatabase.getInstance()
+                .getReference(Common.DRIVER_INFO_REFERENCE)
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .updateChildren(updateData)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(view, e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Snackbar.make(view, "Данные обновлены!", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public static void updateToken(Context context, String token) {
+        TokenModel tokenModel = new TokenModel(token);
+
+        FirebaseDatabase.getInstance()
+                .getReference(Common.TOKEN_REFERENCE)
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .setValue(tokenModel)
+                .addOnFailureListener(e -> Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                });
+
+
+    }
+}
